79ba23f9d1d1767c26bc23bb6388ddca530af9cc
jdargatz <julie.dargatz@gmail.com>
2021-03-22 20:32:38 -0500

    After review, finished studio.

--- studio/rocket.js
+++ studio/rocket.js
@@ -3,6 +3,8 @@ exports.__esModule = true;
 exports.Rocket = void 0;
 var Rocket = /** @class */ (function () {
     function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
     }
@@ -16,25 +18,35 @@ var Rocket = /** @class */ (function () {
     };
     Rocket.prototype.currentMassKg = function () {
         //uses sumMass to return combined mass of this.astronauts and this.cargoItems
-        var number = 0;
-        return number;
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
     };
     Rocket.prototype.canAdd = function (item) {
         //returns true if currentMassKg() + item.massKg  lessthan/equal to totalCapacityKg
         var b = true;
-        return b;
+        return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
     };
     Rocket.prototype.addCargo = function (cargo) {
         //uses canAdd to see if another item can be added
         //if true, adds cargo to cargoItems and returns true
-        var b = true;
-        return b;
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
     };
     Rocket.prototype.addAstronaut = function (astronaut) {
         //uses canAdd to see if another astronaut can be added
         //if true, adds astronaut to this.astronauts and returns true
-        var a = true;
-        return a;
+        //this.canAdd(astronaut) ? this.astronauts.push(astronaut) : return false;
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
     };
     return Rocket;
 }());
--- studio/rocket.ts
+++ studio/rocket.ts
@@ -5,8 +5,8 @@ import { Payload } from "./Payload";
 export class Rocket {
     name: string;
     totalCapacityKg: number;
-    cargoItems: Cargo[];
-    astronauts: Astronaut[];
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
     
     constructor(name: string, totalCapacityKg: number) {
         this.name = name;
@@ -16,37 +16,45 @@ export class Rocket {
 
     sumMass (items: Payload[]): number {
         //returns the sum of all items using each item's massKg
-        let mass = 0;
+        let mass: number = 0;
         for (let i =0; i<items.length;i++ ){
-        mass = mass+items[i].massKg
+        mass = mass+items[i].massKg;
     }
         return mass;
     }
 
     currentMassKg(): number {
         //uses sumMass to return combined mass of this.astronauts and this.cargoItems
-        let number = 0;
-        
-        return number;
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
     }
     
     canAdd(item:Payload):boolean {
         //returns true if currentMassKg() + item.massKg  lessthan/equal to totalCapacityKg
         let b = true;
-        return b;
+        return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
+        
     }
 
     addCargo(cargo:Cargo):boolean {
         //uses canAdd to see if another item can be added
         //if true, adds cargo to cargoItems and returns true
-        let b = true;
-        return b;
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo)
+            return true
+        } else {
+            return false
+        }
     }
 
     addAstronaut(astronaut:Astronaut):boolean {
         //uses canAdd to see if another astronaut can be added
         //if true, adds astronaut to this.astronauts and returns true
-        let a = true;
-        return a;
+        //this.canAdd(astronaut) ? this.astronauts.push(astronaut) : return false;
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut)
+            return true
+        } else {
+            return false
+        }
     }
     }
